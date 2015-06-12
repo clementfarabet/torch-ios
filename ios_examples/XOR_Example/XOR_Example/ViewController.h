f8a388b9a8e4dd7d546112399f78de3c6ee4a611
Kurt Jacobs <mrkurtjacobs@gmail.com>
2015-06-12 02:16:37 +0200

    Initial Commit

---
+++ ios_examples/XOR_Example/XOR_Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XOR_Example
+//
+//  Created by Kurt Jacobs on 2015/06/12.
+//  Copyright (c) 2015 RandomDudes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
